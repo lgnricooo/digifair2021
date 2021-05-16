feeea7c4812eb3775db3ea03d08ddf29cc76a035
DESKTOP-GC2UMSE\63906 <johnricotoribio@gmail.com>
2021-05-16 22:12:17 +0800

    dfghj

--- resources/views/dashboards/admins/opening.blade.php
+++ resources/views/dashboards/admins/opening.blade.php
@@ -292,7 +292,7 @@
     <script src="{{asset('design/assets/js/template.js')}}"></script>
     <!-- end common js -->
     <script type="text/javascript">
-        $(document).ready(function(){
+        $(function(){
           $.ajaxSetup({
 				headers: {
                     'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
@@ -349,7 +349,7 @@
                    
               ]
           });     
-		)})
+		})
     </script>
 
     </body>
