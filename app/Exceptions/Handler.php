33249229e3f8f0f683db0c96422325742612bb54
DESKTOP-GC2UMSE\63906 <johnricotoribio@gmail.com>
2021-05-16 20:18:44 +0800

    asdfasdfasdfadsfasdfasdfasdf

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -4,6 +4,7 @@ namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
+use Illuminate\Auth\AuthenticationException;
 
 class Handler extends ExceptionHandler
 {
@@ -40,11 +41,11 @@ class Handler extends ExceptionHandler
     }
 
     protected function unauthenticated($request, AuthenticationException $exception) 
-{
-    if ($request->expectsJson()) {
-        return response()->json(['error' => 'Unauthenticated.'], 401);
-    }
+    {
+        if ($request->expectsJson()) {
+            return response()->json(['error' => 'Unauthenticated.'], 401);
+        }
 
     return redirect()->guest('login');
-}
+    }
 }
