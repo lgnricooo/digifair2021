268ae5285477fc25f118885f3fd95697348e82db
DESKTOP-GC2UMSE\63906 <johnricotoribio@gmail.com>
2021-05-16 23:33:20 +0800

    dasfasfdas

--- resources/views/dashboards/admins/opening.blade.php
+++ resources/views/dashboards/admins/opening.blade.php
@@ -277,12 +277,7 @@
     <script type="text/javascript" src="https://cdn.datatables.net/1.10.24/js/jquery.dataTables.js"></script>
     <script src="{{asset('design/assets/plugins/feather-icons/feather.min.js')}}"></script>
     <script src="{{asset('design/assets/plugins/perfect-scrollbar/perfect-scrollbar.min.js')}}"></script>
-    <script src="https://cdn.datatables.net/buttons/1.7.0/js/dataTables.buttons.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
-    <script src="https://cdn.datatables.net/buttons/1.7.0/js/buttons.html5.min.js"></script>
-    <script src="https://cdn.datatables.net/buttons/1.7.0/js/buttons.print.min.js"></script>
+    
     <!-- end base js -->
 
     <!-- plugin js -->
