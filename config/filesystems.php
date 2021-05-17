13f053a42b561c5348fa04cb2be3fd18ae6536d1
DESKTOP-GC2UMSE\63906 <johnricotoribio@gmail.com>
2021-05-17 18:48:25 +0800

    aaaaaaaaaaaaaa

--- config/filesystems.php
+++ config/filesystems.php
@@ -35,10 +35,15 @@ return [
             'root' => storage_path('app'),
         ],
 
+        'uploads' => [
+            'driver' => 'local',
+            'root'   => public_path() . '/homeAssets',
+        ],
+
         'public' => [
             'driver' => 'local',
-            'root' => storage_path('app/public'),
-            'url' => env('APP_URL').'/storage',
+            'root' => public_path() . '/homeAssets',
+            'url' => env('APP_URL').'/homeAssets',
             'visibility' => 'public',
         ],
 
